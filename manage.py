8fa1c14e49864b798dad312d21184a98fd8d052c
Evanson <evansonmwangi83@gmail.com>
2016-02-24 15:59:38 +0300

    Added the shell context and unit test methods

--- manage.py
+++ manage.py
@@ -1,11 +1,9 @@
-from app.main.models import User
-from app import create_app, db
+#!/usr/bin/env python
 import os
+from app import create_app, db
+from app.models import User, Role
+from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
-from flask.ext.script import Manager
-
-
-
 
 app = create_app(os.getenv('OPTIMUS_CONFIG') or 'default')
 manager = Manager(app)
@@ -13,37 +11,17 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
-# # migrations
-# manager.add_command('db', MigrateCommand)
 
-
-# @manager.command
-# def create_db():
-#     """Creates the db tables."""
-#     db.create_all()
-
-
-# @manager.command
-# def drop_db():
-#     """Drops the db tables."""
-#     db.drop_all()
-
-
-# @manager.command
-# def create_admin():
-#     """Creates the admin user."""
-#     db.session.add(User(email="evansonmwangi83@gmail.com", password="admin", admin=True))
-#     db.session.commit()
-
-
-# @manager.command
-# def create_data():
-#     """Creates sample data."""
-#     pass
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 if __name__ == '__main__':

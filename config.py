21a5304a7b2ffa3dcbd12cc7978929256b3547d3
Evanson <evansonmwangi83@gmail.com>
2016-02-24 15:58:45 +0300

    Changed the environment variables

--- config.py
+++ config.py
@@ -1,27 +1,18 @@
 import os
-
-# WTF_CSRF_ENABLED = True
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-# required by the Flask-SQLAlchemy extension.
-
 class Config:
-    SECRET_KEY = 'my_secret'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'optimus.sqlite')
-    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'd3vm4r5'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    OPTIMUS_MAIL_SUBJECT_PREFIX = '[Optimus]'
-    OPTIMUS_MAIL_SENDER = 'Optimus Admin <evansonmwangi83@gmail.com>'
-    OPTIMUS_ADMIN = os.environ.get('OPTIMUS_ADMIN')
-    TEST_DATABASE_URL = 'sqlite:///' + os.path.join(basedir, 'test_optimus.sqlite')
-    DEV_DATABASE_URL = 'sqlite:///' + os.path.join(basedir, 'dev_optimus.sqlite')
-    PROD_DATABASE_URL = 'sqlite:///' + os.path.join(basedir, 'prod_optimus.sqlite')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Optimus]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('OPTIMUS_ADMIN')
 
     @staticmethod
     def init_app(app):
@@ -30,22 +21,25 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = Config.DEV_DATABASE_URL
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = Config.TEST_DATABASE_URL
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = Config.PROD_DATABASE_URL
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
+
     'default': DevelopmentConfig
 }
